3dcc60b6e53301cf7b92cc96988a274a0ba0c854
Akhileshmalik23 <akhileshmalik908@gmail.com>
2023-10-15 23:45:18 +0530

    add-javaScript-code

---
+++ tut53.js
@@ -0,0 +1,15 @@
+console.log("this is tut53.htm")
+
+let name1 ="akhilesh";
+let name2 = "bla";
+let name3 = "oya";
+
+// console.log(name1+ " is a good")
+// console.log(name2 +"+ is a good")
+// console.log(name3 +"is a good")
+ function good(nme){
+    console.log(nme+ " is a good")
+ }
+ good(name1)
+ good(name2)
+ good(name3)
